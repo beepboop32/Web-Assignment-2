75572d8b0d4a898e2d73a7c39b5cb92b7197e0c6
beepboop32 <157918068+beepboop32@users.noreply.github.com>
2025-01-24 09:41:39 +0000

    bap

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ window.onload()= function() {
         document.getElementById
     }, 1000);
 }
+
+function cats(event, carName) {
+    let i, tabcontent, tabs;
+
+    tabcontent = document.getElementsByClassName("tablinks");
+    for (i=0; i<tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tabcontent = document.getElementsByClassName("tablinks");
+    for (i=0; i<tabcontent.length; i++) {
+        tabs[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(carName).style.display = "block";
+    event.currentTarget.className += " active";
+    
+
+
+}
