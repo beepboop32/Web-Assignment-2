aebdc3ec03f6179cdac19889b9bc79dd4cc545de
beepboop32 <157918068+beepboop32@users.noreply.github.com>
2025-02-03 14:06:55 +0000

    wowow

--- mrMunchkinsMan/mrMunchkinsMan.js
+++ mrMunchkinsMan/mrMunchkinsMan.js
@@ -1,4 +1,7 @@
-console.log("mr munchkins man")
+let audios = [];
+
+
+
 function openNav() {
     document.getElementById("sideNav").style.width = "250px";
 }
@@ -8,3 +11,16 @@ function closeNav() {
 window.onload = function() {
     document.getElementById("MunchkinMan").style.backgroundImage = "mrMunchkinsMan/mrMunchkinsMan.png"
 }
+let audio1 = audios.push(new Audio("larryCream.mp3"));
+let audio2 = audios.push(new Audio("4BigGuys.mp3"));
+let audio3 = audios.push(new Audio("evilBurger.mp3"));
+function meowmeow() {
+    let random = Math.floor(Math.random() * audios.length);
+    console.log(random);
+    let audio = audios[random];
+    audio.play();
+    audio.onended = function() {
+        meowmeow();
+    };
+};
+meowmeow();
